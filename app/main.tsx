40ea295143ee67765261e9db1463b53d532b83c5
Jeremy <jeremy@matt-edv.com>
2025-07-28 15:04:50 +0200

    Generelle Verbesserungen und Veränderungen

--- app/main.tsx
+++ app/main.tsx
@@ -1,24 +1,22 @@
+// app/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import {
-    BrowserRouter,
-    Routes,
-    Route
-} from "react-router-dom";
-import Root     from "./root";
-import Home     from "./routes/index";
-import Tickets  from "./routes/tickets";
-import Geraete  from "./routes/geraete";
-import EditDevice from "./routes/geraete-bearbeiten";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById("root")!).render(
+import Root from "./root";
+import Home from "./routes/index";
+import GeraetePage from "./routes/geraete";
+import TicketsPage from "./routes/tickets";
+import "./app.css";
+
+const container = document.getElementById("root")!;
+createRoot(container).render(
     <BrowserRouter>
         <Routes>
-            <Route element={<Root />}>
+            <Route path="/" element={<Root />}>
                 <Route index element={<Home />} />
-                <Route path="tickets" element={<Tickets />} />
-                <Route path="geraete" element={<Geraete />} />
-                <Route path="geraete/bearbeiten/:id" element={<EditDevice />} />
+                <Route path="geraete" element={<GeraetePage />} />
+                <Route path="tickets" element={<TicketsPage />} />
             </Route>
         </Routes>
     </BrowserRouter>
